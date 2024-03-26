3ef91d63d99c9b5ddd275c79d64b87866ada6652
Rafael Freitas <oraffaudi@gmail.com>
2024-03-26 01:37:27 -0300

    first commit

---
+++ src/app/component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { Model } from "./repository.model";
+
+
+@Component({
+    selector: "app",
+    templateUrl: "template.html"
+})
+
+export class ProductComponent {
+    model: Model = new Model();
+}
