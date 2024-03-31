394383e5808b7881c15cafc57ebf99e055d6fbbc
Rafael Freitas <oraffaudi@gmail.com>
2024-03-31 20:28:45 -0300

    reading a single host element attribute

--- src/app/attr.directive.ts
+++ src/app/attr.directive.ts
@@ -1,11 +1,18 @@
-import { Directive, ElementRef } from "@angular/core";
+import { Directive, ElementRef, Attribute } from "@angular/core";
 
 
 @Directive({
     selector: "[pa-attr]",
 })
 export class PaAttrDirective {
-    constructor(element: ElementRef) {
-        element.nativeElement.classList.add("table-success", "fw-bold");
+
+
+    // constructor(element: ElementRef) {
+    //     element.nativeElement.classList.add("table-success", "fw-bold");
+    // }
+
+    constructor(element: ElementRef, @Attribute("pa-attr-class") bgClass: string) {
+        console.log(bgClass);
+        element.nativeElement.classList.add(bgClass || "table-success", "fw-bold");   
     }
-}
+}    
