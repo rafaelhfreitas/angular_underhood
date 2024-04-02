9c42c38fe997546a4c1175782473d5c163139c17
Rafael Freitas <oraffaudi@gmail.com>
2024-04-01 21:46:24 -0300

    defining multiline template

--- src/app/productTable.component.ts
+++ src/app/productTable.component.ts
@@ -3,7 +3,9 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "paProductTable",
-    template: "<div>This is the table component</div>"
+    template: `<div class='bg-info p-2'>
+                    This is a multiline template
+                </div>`
 })
 export class ProductTableComponent {
 }
