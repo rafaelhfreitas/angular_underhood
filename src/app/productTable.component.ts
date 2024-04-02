790dc92b807fe75384e8b9cffec6664bb06fe82f
Rafael Freitas <oraffaudi@gmail.com>
2024-04-01 21:49:23 -0300

    Creating a external template

--- src/app/productTable.component.ts
+++ src/app/productTable.component.ts
@@ -3,9 +3,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "paProductTable",
-    template: `<div class='bg-info p-2'>
-                    This is a multiline template
-                </div>`
+    templateUrl: "productTable.component.html"
 })
 export class ProductTableComponent {
 }
