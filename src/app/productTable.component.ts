e894278edce0dfcbf2d57fb88ff0f9bc94089e44
Rafael Freitas <oraffaudi@gmail.com>
2024-04-01 23:49:42 -0300

    selecting values with i18nSelect pipe

--- src/app/productTable.component.ts
+++ src/app/productTable.component.ts
@@ -31,10 +31,15 @@ export class ProductTableComponent {
     categoryFilter: string | undefined;
     itemCount: number = 3;
 
+    // dateObject: Date = new Date(2020, 1, 20);
+    // dateString: string = "2020-02-20T00:00:00.000Z";
+    // dateNumber: number = 1582156800000;
 
-    dateObject: Date = new Date(2020, 1, 20);
-    dateString: string = "2020-02-20T00:00:00.000Z";
-    dateNumber: number = 1582156800000;
+    selectMap = {
+        "Watersports": "stay dry",
+        "Soccer": "score goals",
+        "other": "have fun"
+    }
 
 
 }
