5b9cb27842a623c31273de963229ace9b270ef95
Rafael Freitas <oraffaudi@gmail.com>
2024-04-03 21:28:52 -0300

    using view child provider

--- src/app/productForm.component.ts
+++ src/app/productForm.component.ts
@@ -7,7 +7,7 @@ import { VALUE_SERVICE } from "./valueDisplay.directive";
 @Component({
     selector: "pa-productform",
     templateUrl: "productForm.component.html",
-    providers: [{ provide: VALUE_SERVICE, useValue: "Oranges"}]
+    viewProviders: [{ provide: VALUE_SERVICE, useValue: "Oranges"}]
 })
 export class ProductFormComponent {
 
